a38b3153e6bd2ff9a1a558f303e64ae8fd1731be
Sergei Ivanov <9153104109@mail.ru>
2017-10-06 20:58:23 +0300

    рефакторинг кода: добавил вспомогательный класс HelperBase

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by User on 06.10.2017.
+ */
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getContactHelper().returnToHomePage();
+    }
+}
