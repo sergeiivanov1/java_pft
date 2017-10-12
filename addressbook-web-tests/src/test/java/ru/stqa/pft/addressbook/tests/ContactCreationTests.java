1ba6cd3b5367e302cc22417c843b4f123f43b70f
Sergei Ivanov <9153104109@mail.ru>
2017-10-13 00:08:10 +0300

    модифицированы количественные проверки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,17 +4,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
-        int before = app.getContactHelper().getContactCount();
+        List<ContactData> before = app.getContactHelper().getContactList();
         app.getNavigationHelper().gotoAddContactPage();
         app.getContactHelper().createContact(new ContactData("name2","middlename2","wefw",
                 "111122","223323","33333333","gwggwr@ewfw.ru","wefw@mail.ru","rffw","efwef","test1"));
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after,before + 1);
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(),before.size() + 1);
     }
 
 }
