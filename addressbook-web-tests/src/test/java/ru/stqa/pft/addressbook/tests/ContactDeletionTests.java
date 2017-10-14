90a1dfc245b42169d3608cfac68e9663f56cd4eb
Sergei Ivanov <9153104109@mail.ru>
2017-10-14 12:30:26 +0300

    добавлены проверки списков для тестов Модификации и Создания групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 /**
  * Created by User on 09.10.2017.
  */
@@ -13,17 +15,20 @@ public class ContactDeletionTests extends TestBase{
     @Test
     public void testContactDeletion() {
         app.getNavigationHelper().gotoHomePage();
-        int before = app.getContactHelper().getContactCount();
+
         if (! app.getContactHelper().isThereAContact()) {
             app.getNavigationHelper().gotoAddContactPage();
             app.getContactHelper().createContact(new ContactData("name2","middlename2","wefw",
                     "111122","223323","33333333","gwggwr@ewfw.ru","wefw@mail.ru","rffw","efwef","test1"));
             app.getNavigationHelper().gotoHomePage();
         }
-        app.getContactHelper().selectContact(before - 1);
+        List<ContactData> before= app.getContactHelper().getContactList();
+        app.getContactHelper().selectContact(before.size() - 1);
         app.getContactHelper().deleteSelectedContacts();
         app.getContactHelper().confirmationContactDeletion();
-        int after = app.getContactHelper().getContactCount();
-        Assert.assertEquals(after,before - 1);
+        List<ContactData> after= app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(),before.size() - 1);
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before,after);
     }
 }
